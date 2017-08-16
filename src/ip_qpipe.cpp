247aeb1220fa9048dd36e1804334142bf038fa88
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2017-08-16 20:48:48 +0700

    first step of 'T_IP_PipeView' and 'T_IP_PipeViewTx' classes impl.

--- src/ip_qpipe.cpp
+++ src/ip_qpipe.cpp
@@ -1,13 +1,58 @@
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 
-#include <QtGlobal>
 #include <QDebug>
-
-//------------------------------------------------------------------------------
 
 #include "ip_qpipe.h"
 
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+T_IP_PipeView::T_IP_PipeView(const QString& key) :
+                                                   mSharedMemory(key),
+                                                   mChunkSize(0),
+                                                   mChunkNum(0)
+{
+    #if defined(IP_QPIPE_PRINT_DEBUG_INFO)
+        qDebug() << "[INFO] [T_IP_PipeView constructor]   key:" << key;
+    #endif
+}
+
+//------------------------------------------------------------------------------
+T_IP_PipeView::~T_IP_PipeView()
+{
+    #if defined(IP_QPIPE_PRINT_DEBUG_INFO)
+        qDebug() << "[INFO] [T_IP_PipeView destructor]    key:" << mSharedMemory.key();
+    #endif
+}
 
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+T_IP_PipeViewTx::T_IP_PipeViewTx(const QString& key, uint32_t chunkSize, uint32_t chunkNum) : T_IP_PipeView(key)
+{
+    mChunkSize = chunkSize;
+    mChunkNum  = chunkNum;
+
+    if(!mSharedMemory.create(mChunkSize*mChunkNum + sizeof(T_IP_PipeView::TPipeControlBlock),QSharedMemory::ReadWrite)) {
+        #if defined(IP_QPIPE_PRINT_DEBUG_ERROR)
+            qDebug() << "[ERROR] [T_IP_PipeViewTx constructor] at QSharedMemory::create";
+        #endif
+    }
+
+    #if defined(IP_QPIPE_PRINT_DEBUG_INFO)
+        qDebug() << "[INFO] [T_IP_PipeViewTx constructor] key:" << key << "chunkSize:" << chunkSize << "chunkNum:" << chunkNum;
+        qDebug() << "size:" << mSharedMemory.size();
+    #endif
+}
+
+//------------------------------------------------------------------------------
+T_IP_PipeViewTx::~T_IP_PipeViewTx()
+{
+    #if defined(IP_QPIPE_PRINT_DEBUG_INFO)
+        qDebug() << "[INFO] [T_IP_PipeViewTx destructor]  key:" << mSharedMemory.key();
+    #endif
+}
+
--- src/ip_qpipe.h
+++ src/ip_qpipe.h
@@ -3,8 +3,49 @@
 #if !defined(IP_QPIPE_H)
 #define IP_QPIPE_H
 
+#include <cstdint>
+#include <QSharedMemory>
+
+#define IP_QPIPE_PRINT_DEBUG_INFO
+#define IP_QPIPE_PRINT_DEBUG_ERROR
+
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
+class T_IP_PipeView
+{
+    public:
+        T_IP_PipeView(const QString& key);
+        virtual ~T_IP_PipeView() = 0;
+
+    protected:
+        static const size_t PageSize = 4096; // check at Linux
+
+        struct TPipeControlBlock
+        {
+            uint32_t rdyTx;
+            uint32_t rdyRx;
+            uint32_t writeIdx;
+            uint32_t chunkFilled;
+            uint32_t chunkSize;
+            uint32_t chunkNum;
+            uint8_t  reserved[PageSize-6*sizeof(uint32_t)];
+        };
+
+        QSharedMemory mSharedMemory;
+        uint32_t      mChunkSize;
+        uint32_t      mChunkNum;
+};
+
+//------------------------------------------------------------------------------
+//------------------------------------------------------------------------------
+class T_IP_PipeViewTx : public T_IP_PipeView
+{
+    public:
+        T_IP_PipeViewTx(const QString& key, uint32_t chunkSize, uint32_t chunkNum);
+        ~T_IP_PipeViewTx();
+
+    protected:
+};
 
 #endif /* IP_QPIPE_H */
 
