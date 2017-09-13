83a3118aeef2d2819a214f3ba98c80248003382c
Mike Y. Tsivinsky <m_tsiv@mail.ru>
2017-09-13 09:30:01 +0700

    fix bugs in clean.bat (path names)

--- clean.bat
+++ clean.bat
@@ -11,13 +11,13 @@ if "%1"=="" (
 
 del %TOPDIR%\Makefile.*
 del %TOPDIR%\src\Makefile.*
-del %TOPDIR%\test\test_rx\Makefile.*
-del %TOPDIR%\test\test_tx\Makefile.*
+del %TOPDIR%\test\test_rx_pipe\Makefile.*
+del %TOPDIR%\test\test_tx_pipe\Makefile.*
 
 del %TOPDIR%\*.sln
 del %TOPDIR%\src\*.vcxproj.*
-del %TOPDIR%\test\test_rx\*.vcxproj.*
-del %TOPDIR%\test\test_tx\*.vcxproj.*
+del %TOPDIR%\test\test_rx_pipe\*.vcxproj.*
+del %TOPDIR%\test\test_tx_pipe\*.vcxproj.*
 
 
 rd  %TOPDIR%\build /S /Q 
@@ -27,15 +27,15 @@ if %DISABLE_QMAKE_DEFAULT_BUILD%==0 (
 	rd  %TOPDIR%\src\debug /S /Q 
 	rd  %TOPDIR%\src\release /S /Q
 	
-	rd  %TOPDIR%\test\test_rx\debug /S /Q 
-	rd  %TOPDIR%\test\test_rx\release /S /Q
+	rd  %TOPDIR%\test\test_rx_pipe\debug /S /Q 
+	rd  %TOPDIR%\test\test_rx_pipe\release /S /Q
 
-	rd  %TOPDIR%\test\test_tx\debug /S /Q 
-	rd  %TOPDIR%\test\test_tx\release /S /Q
+	rd  %TOPDIR%\test\test_tx_pipe\debug /S /Q 
+	rd  %TOPDIR%\test\test_tx_pipe\release /S /Q
 )
 
 if %USE_VS_BUILD%==1 (
 	rd  %TOPDIR%\src\x64 /S /Q
-	rd  %TOPDIR%\test\test_rx\x64 /S /Q
-	rd  %TOPDIR%\test\test_tx\x64 /S /Q
+	rd  %TOPDIR%\test\test_rx_pipe\x64 /S /Q
+	rd  %TOPDIR%\test\test_tx_pipe\x64 /S /Q
 )
